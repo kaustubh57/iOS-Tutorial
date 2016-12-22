636110337c5c134d425afcd420103031f5cbce91
Kaustubh <kaustubh.kesarkar@gmail.com>
2016-12-21 19:35:22 -0800

    Adds second screen to the app. Embedded app in the `Navigation Controller`. On click of button from 1st screen navigate to the second screen.

---
+++ iOS-Tutorial/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  iOS-Tutorial
+//
+//  Created by Kaustubh Kesarkar on 12/21/16.
+//  Copyright © 2016 com.devkau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
